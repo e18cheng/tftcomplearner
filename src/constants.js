7777bc5ad5cccd657d22fd071392aeaa8fcf34a1
Eric Cheng <echeng41@gatech.edu>
2021-09-15 22:49:26 -0400

    add public images and constants

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const ChampionData = require("./tft_data/champions.json")
+
+export const MetaComps = [
+    "Lucian",
+    "Kayle",
+    "Rangers",
+    "Nocturne",
+    "Fortnite Jax",
+    "Abom + X",
+    "Draven",
+    "Vayne Reroll",
+  ]
